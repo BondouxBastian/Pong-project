f035409bae58dde56fb8f820a1d23f6b6774431f
bastianbondoux <bastian.bondoux@epitech.eu>
2026-07-14 18:34:30 +0200

    add Room game loop with state broadcast

--- server/lib/Room.js
+++ server/lib/Room.js
@@ -1,5 +1,6 @@
 const GameState = require('./GameState');
 const MESSAGE_TYPES = require('./messageTypes');
+const { TICK_RATE } = require('./constants');
 
 let nextRoomId = 1;
 
@@ -10,6 +11,7 @@ class Room {
     this.spectators = new Set();
     this.gameState = new GameState();
     this.started = false;
+    this.loopHandle = null;
   }
 
   isFull() {
@@ -34,9 +36,43 @@ class Room {
   removeConnection(connection) {
     if (connection.side === 'left' || connection.side === 'right') {
       delete this.players[connection.side];
+      this.stopLoop();
       this.broadcast({ type: MESSAGE_TYPES.OPPONENT_DISCONNECTED });
     } else {
       this.spectators.delete(connection);
+    }
+  }
+
+  startIfReady() {
+    if (this.isFull() && !this.started) {
+      this.started = true;
+      this.broadcast({ type: MESSAGE_TYPES.ROOM_READY });
+      this.startLoop();
+    }
+  }
+
+  startLoop() {
+    if (this.loopHandle) return;
+    const intervalMs = 1000 / TICK_RATE;
+    this.loopHandle = setInterval(() => this.tick(), intervalMs);
+  }
+
+  stopLoop() {
+    if (this.loopHandle) {
+      clearInterval(this.loopHandle);
+      this.loopHandle = null;
+    }
+    this.started = false;
+  }
+
+  tick() {
+    this.gameState.update();
+    this.broadcast({ type: MESSAGE_TYPES.GAME_STATE, state: this.gameState.toSnapshot() });
+
+    if (this.gameState.winner) {
+      this.broadcast({ type: MESSAGE_TYPES.GAME_OVER, winner: this.gameState.winner });
+      this.onGameOver && this.onGameOver(this.gameState.winner);
+      this.stopLoop();
     }
   }
 
