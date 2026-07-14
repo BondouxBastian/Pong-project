5b115cd6573b2fa65d35cd32bb11fcdd38729be6
bastianbondoux <bastian.bondoux@epitech.eu>
2026-07-14 18:32:58 +0200

    add Room spectator handling and disconnect broadcast

--- server/lib/Room.js
+++ server/lib/Room.js
@@ -1,4 +1,5 @@
 const GameState = require('./GameState');
+const MESSAGE_TYPES = require('./messageTypes');
 
 let nextRoomId = 1;
 
@@ -22,6 +23,31 @@ class Room {
     connection.side = side;
     connection.room = this;
     return side;
+  }
+
+  addSpectator(connection) {
+    this.spectators.add(connection);
+    connection.room = this;
+    connection.side = 'spectator';
+  }
+
+  removeConnection(connection) {
+    if (connection.side === 'left' || connection.side === 'right') {
+      delete this.players[connection.side];
+      this.broadcast({ type: MESSAGE_TYPES.OPPONENT_DISCONNECTED });
+    } else {
+      this.spectators.delete(connection);
+    }
+  }
+
+  isEmpty() {
+    return !this.players.left && !this.players.right && this.spectators.size === 0;
+  }
+
+  broadcast(message) {
+    const payload = JSON.stringify(message);
+    Object.values(this.players).forEach((conn) => conn && conn.send(payload));
+    this.spectators.forEach((conn) => conn.send(payload));
   }
 }
 
